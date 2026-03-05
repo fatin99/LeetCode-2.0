13c1bd429b438d5a132d6164ae1cb17fcc6dce38
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-05 21:24:22 +0800

    reorder-list

---
+++ Linked List/reorder-list.py
@@ -0,0 +1,142 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+
+    # Time: O(n)
+    # Space: O(n)
+    # def reorderList(self, head: Optional[ListNode]) -> None:
+    #     """
+    #     Do not return anything, modify head in-place instead.
+    #     """
+    #     nodeList = []
+    #     tail = head
+    #     while tail:
+    #         nodeList.append(tail)
+    #         tail = tail.next
+
+    #     length = len(nodeList)
+    #     curr = head
+
+    #     while len(nodeList) > length / 2 + 1:
+    #         tail = nodeList[-1]
+    #         temp = curr.next
+    #         curr.next = tail
+    #         tail.next = temp
+            
+    #         curr = curr.next
+    #         curr = curr.next
+            
+    #         nodeList.pop()
+
+    #     if length / 2 + 1 == len(nodeList):
+    #         curr = curr.next
+    #         curr.next = None
+    #     else:
+    #         curr.next = None
+    
+    # Time: O(n^2)
+    # Space: O(1)
+    # def reorderList(self, head: Optional[ListNode]) -> None:
+    #     """
+    #     Do not return anything, modify head in-place instead.
+    #     """
+    #     tail = head
+    #     length = 0
+    #     while tail.next:
+    #         length += 1
+    #         tail = tail.next
+
+    #     front = head
+    #     back = tail
+        
+    #     currLength = length - 1
+    #     while currLength > 0:
+    #         temp = front.next
+    #         front.next = back
+    #         back.next = temp
+
+    #         front = front.next
+    #         front = front.next
+
+    #         for _ in range(currLength):
+    #             back = back.next
+    #         currLength -= 2
+
+    #     if currLength == 0:
+    #         front.next = back
+    #         back.next = None
+    #     else:
+    #         front.next = None
+            
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        tail = head
+        mid = head
+        while tail.next:
+            mid = mid.next
+            if tail.next.next:
+                tail = tail.next.next
+            elif tail.next:
+                tail = tail.next
+        
+        
+        #reverse second half
+        prev = None
+        curr = mid
+
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        
+        front2 = prev
+        front1 = head
+        
+        # combine first and second half
+        while front2:
+            temp1 = front1.next
+            temp2 = front2.next
+
+            front1.next = front2
+            front2.next = temp1
+            if not temp2 and not temp1:                
+                front2.next = None
+            if not temp2 and temp1:
+                temp1.next = None
+            front2 = temp2
+            front1 = temp1
+        return head
+             
+
+def list_to_linked_list(items):
+    if not items:
+        return None
+
+    head = ListNode(items[0])
+    current = head
+
+    for value in items[1:]:
+        current.next = ListNode(value)
+        current = current.next
+
+    return head
+
+
+lst = [1, 2, 3, 4, 5, 6]
+linked_list = list_to_linked_list(lst)
+print(Solution().reorderList(linked_list))
+
+lst = [1, 2, 3, 4, 5]
+linked_list = list_to_linked_list(lst)
+print(Solution().reorderList(linked_list))
+
+lst = [1,2,3,4]
+linked_list = list_to_linked_list(lst)
+print(Solution().reorderList(linked_list))
+print()
