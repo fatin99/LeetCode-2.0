78723c7fef751eadf4a775026336e18395460464
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-09 18:08:44 +0800

    validate-binary-search-tree

--- Trees/validate-binary-search-tree.py
+++ Trees/validate-binary-search-tree.py
@@ -7,31 +7,89 @@ class TreeNode:
 from collections import deque
 
 class Solution:
+    # def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    #     queue = deque()
+    #     if root:
+    #         queue.append((root, []))
+    #     while queue:
+    #         for _ in range(len(queue)):
+    #             node, path = queue.popleft()
+    #             if node.left:
+    #                 if node.val <= node.left.val:
+    #                     return False
+    #                 queue.append((node.left, path + [(node, True, False)]))
+    #             if node.right:
+    #                 if node.val >= node.right.val:
+    #                     return False
+    #                 queue.append((node.right, path + [(node, False, True)])) 
+    #             for ancestor in path:
+    #                 parent = ancestor[0]
+    #                 left = ancestor[1]
+    #                 right = ancestor[2]
+    #                 if left:
+    #                     if node.val >= parent.val:
+    #                         return False
+    #                 if right:
+    #                     if node.val <= parent.val:
+    #                         return False
+    #     return True
+
+    # DFS
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        if root:
+            stack.append((root, None, None))
+        while stack:
+            node, minimum, maximum = stack.pop()
+            if minimum:
+                if node.val <= minimum:
+                    return False
+            if maximum:
+                if node.val >= maximum:
+                    return False
+            if node.left:
+                if node.val <= node.left.val:
+                    return False
+                if maximum:
+                    stack.append((node.left, minimum, min(maximum, node.val)))
+                else:
+                    stack.append((node.left, minimum, node.val))
+            if node.right:
+                if node.val >= node.right.val:
+                    return False
+                if minimum:
+                    stack.append((node.right, max(minimum, node.val), maximum)) 
+                else:
+                    stack.append((node.right, node.val, maximum)) 
+        return True
+
+    # BFS
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         queue = deque()
         if root:
-            queue.append((root, []))
+            queue.append((root, None, None))
         while queue:
-            for _ in range(len(queue)):
-                node, path = queue.popleft()
-                if node.left:
-                    if node.val <= node.left.val:
-                        return False
-                    queue.append((node.left, path + [(node, True, False)]))
-                if node.right:
-                    if node.val >= node.right.val:
-                        return False
-                    queue.append((node.right, path + [(node, False, True)])) 
-                for ancestor in path:
-                    parent = ancestor[0]
-                    left = ancestor[1]
-                    right = ancestor[2]
-                    if left:
-                        if node.val >= parent.val:
-                            return False
-                    if right:
-                        if node.val <= parent.val:
-                            return False
+            node, minimum, maximum = queue.popleft()
+            if minimum:
+                if node.val <= minimum:
+                    return False
+            if maximum:
+                if node.val >= maximum:
+                    return False
+            if node.left:
+                if node.val <= node.left.val:
+                    return False
+                if maximum:
+                    queue.append((node.left, minimum, min(maximum, node.val)))
+                else:
+                    queue.append((node.left, minimum, node.val))
+            if node.right:
+                if node.val >= node.right.val:
+                    return False
+                if minimum:
+                    queue.append((node.right, max(minimum, node.val), maximum)) 
+                else:
+                    queue.append((node.right, node.val, maximum)) 
         return True
 
 def to_binary_tree(items):
@@ -56,4 +114,7 @@ root = to_binary_tree([5,4,6,None,None,3,7])
 print(Solution().isValidBST(root))
 
 root = to_binary_tree([3,1,5,0,2,4,6])
+print(Solution().isValidBST(root))
+
+root = to_binary_tree([32,26,47,19,None,None,56,None,27])
 print(Solution().isValidBST(root))
