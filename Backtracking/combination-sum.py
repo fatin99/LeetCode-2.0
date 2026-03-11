8632226f393f8af9bb8465ee20c0ca97c4867963
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-11 17:16:32 +0800

    combination-sum

--- Backtracking/combination-sum.py
+++ Backtracking/combination-sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    #recursive
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
 
@@ -6,7 +7,7 @@ class Solution:
             if currSum == target:
                 res.append(list(currNums))
                 return
-            while index in range(len(candidates)):
+            while index < len(candidates):
                 num = candidates[index]
                 newSum = currSum + num
                 if newSum > target:
@@ -18,6 +19,29 @@ class Solution:
                 index += 1
 
         dfs(0, [], 0)
+        return res
+    
+    #iterative
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        stack = [(0, [], 0)]
+        
+        while stack:
+            currSum, currNums, index = stack.pop()
+            
+            if currSum == target:
+                res.append(list(currNums))
+                continue
+            
+            while index < len(candidates):
+                num = candidates[index]
+                newSum = currSum + num
+                if newSum > target:
+                    index += 1
+                    continue
+                currNums.append(num)
+                stack.append((newSum, currNums + [num], index))
+                index += 1
         return res
 
 print(Solution().combinationSum([2,3,6,7], 7))
