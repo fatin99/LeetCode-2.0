ce1b6a569694b5365eb7d86093c24f663603a22a
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-13 16:12:58 +0800

    clone-graph

---
+++ Graphs/clone-graph.py
@@ -0,0 +1,35 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+class Solution:
+    # DFS
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        visited = set()
+        stack = []
+        newNode = None
+        created = {}
+        if node:
+            newNode = Node(node.val)
+            stack.append((node, newNode))
+            created[node.val] = newNode
+
+        while stack:
+            current, newCurrent = stack.pop()
+            for neighbor in current.neighbors:
+                if neighbor.val in created:
+                    newNeighbor = created[neighbor.val]
+                else:
+                    newNeighbor = Node(neighbor.val)
+                    created[neighbor.val] = newNeighbor
+                if not current in visited:
+                    newCurrent.neighbors.append(newNeighbor)
+                if not neighbor in visited:
+                    stack.append((neighbor, newNeighbor))
+            if not current in visited:
+                visited.add(current)
+
+        return newNode
