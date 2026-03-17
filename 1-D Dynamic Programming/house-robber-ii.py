20ca3da871eca19ade08128bda5b6c856177a3ff
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-17 17:25:13 +0800

    house-robber-ii

--- 1-D Dynamic Programming/house-robber-ii.py
+++ 1-D Dynamic Programming/house-robber-ii.py
@@ -6,21 +6,45 @@ class Solution:
             prev_max0 = nums[0]
             prev_max1 = nums[1]
 
-            for i in range(2, len(nums)):
+            for i in range(2, len(nums)-1):
                 # dp0: only update for i < len(nums)-1 (exclude last house)
-                if i < len(nums) - 1:
-                    currMax = nums[i] + prev_max0
-                    prev_max0 = max(prev_max0, dp0[-1])
-                    dp0.append(currMax)
+                currMax = nums[i] + prev_max0
+                prev_max0 = max(prev_max0, dp0[-1])
+                dp0.append(currMax)
 
                 # dp1: only update for i >= 2 but using nums[i] offset by 1 (exclude first house)
-                if i + 1 < len(nums):
-                    currMax = nums[i+1] + prev_max1
-                    prev_max1 = max(prev_max1, dp1[-1])
-                    dp1.append(currMax)
+                currMax = nums[i+1] + prev_max1
+                prev_max1 = max(prev_max1, dp1[-1])
+                dp1.append(currMax)
 
             max0 = max(dp0[-1], dp0[-2])
             max1 = max(dp1[-1], dp1[-2])
+            return max(max0, max1)
+        
+        else:
+            return max(nums)
+        
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) > 2:
+            prev0 = nums[1]
+            prev_prev0 = nums[0]
+            prev1 = nums[2]
+            prev_prev1 = nums[1]
+            
+            prevMax0 = prev_prev0
+            prevMax1 = prev_prev1
+            
+            for i in range(2, len(nums)-1):
+                curr = nums[i] + prevMax0
+                prevMax0 = max(prevMax0, prev0)
+                prev_prev0, prev0 = prev0, curr
+
+                curr = nums[i+1] + prevMax1
+                prevMax1 = max(prevMax1, prev1)
+                prev_prev1, prev1 = prev1, curr
+            
+            max0 =  max(prev_prev0, prev0)
+            max1 =  max(prev_prev1, prev1)
             return max(max0, max1)
         
         else:
--- 1-D Dynamic Programming/house-robber.py
+++ 1-D Dynamic Programming/house-robber.py
@@ -2,6 +2,7 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) > 1:
             dp = [nums[0], nums[1]]
+            
             prevMax = nums[0]  # tracks max of dp[0..i-2]
             
             for i in range(2, len(nums)):
@@ -13,6 +14,24 @@ class Solution:
         
         else:
             return max(nums)
+        
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) > 1:
+            prev = nums[1]
+            prev_prev = nums[0]
+            
+            prevMax = prev_prev
+            
+            for i in range(2, len(nums)):
+                curr = nums[i] + prevMax
+                prevMax = max(prevMax, prev)
+                prev_prev, prev = prev, curr
+            
+            return max(prev, prev_prev)
+        
+        else:
+            return max(nums)
+
 
 print(Solution().rob([1,2,3,1]))
 print(Solution().rob([2,7,9,3,1]))
