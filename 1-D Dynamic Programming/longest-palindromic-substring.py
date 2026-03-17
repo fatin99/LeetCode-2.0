3e3e88961c83d577bd82c469cb2210c63ae962be
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-17 19:31:24 +0800

    longest-palindromic-substring

---
+++ 1-D Dynamic Programming/longest-palindromic-substring.py
@@ -0,0 +1,56 @@
+class Solution:
+    # O(n^2)
+    def longestPalindrome(self, s: str) -> str:
+        maxPalindrome = s[0]
+        dp = []
+        
+        for i in range(len(s)-1):
+            odd = (i, i) #centre of an odd length palindrome (e.g. babab)
+            even = (i, i + 1) #centre of an even length palindrome (e.g. cddc)
+            for start, end in [odd, even]:
+                
+                while start >= 0 and end <= len(s)-1 and s[start] == s[end]:
+                    start -= 1
+                    end += 1
+                    currStr = s[start+1:end]
+                
+                if len(currStr) > len(maxPalindrome):
+                    maxPalindrome = currStr
+                dp.append(currStr)
+        
+        print(dp)
+        return maxPalindrome
+    
+    # DP
+    # def longestPalindrome(self, s: str) -> str:
+    #     def isPalindrome(s: str) -> bool:
+    #         # char is left pointer, back is right pointer
+    #         back = len(s) - 1
+    #         for char in s:
+    #             if not char.isalnum():
+    #                 continue
+    #             while not s[back].isalnum():
+    #                 back -= 1
+    #             # print(f"Comparing {char.lower()} and {s[back].lower()}")
+    #             if char.lower() == s[back].lower():
+    #                 back -= 1
+    #                 continue
+    #             else:
+    #                 return False
+    #         return True
+    
+    #     maxPalindrome = s[0]
+    #     for i in range(len(s)):
+    #         end = s[i]
+    #         for j in range(i-len(maxPalindrome)+1):
+    #             start = s[j]
+    #             currStr = s[j:i+1]
+    #             if start == end and isPalindrome(currStr):
+    #                 maxPalindrome = currStr
+    #                 break
+    #     return maxPalindrome
+    
+print(Solution().longestPalindrome("babad"))
+print(Solution().longestPalindrome("cbbd"))
+print(Solution().longestPalindrome("cbbc"))
+print(Solution().longestPalindrome("abbcccba"))
