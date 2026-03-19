dd0965e1259a19602c34fc6d55a3c1643e854c04
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-19 20:56:41 +0800

    coin-change

--- 1-D Dynamic Programming/coin-change.py
+++ 1-D Dynamic Programming/coin-change.py
@@ -1,38 +1,78 @@
+from collections import deque
+
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
+
+        coins.sort(reverse=True) # O(1), 1 <= coins.length <= 12
         if coins[-1] > amount:
             return -1
+        if len(coins) == 1 and coins[0] > amount:
+            return -1
+
+        prevSums = [0]
         
-        coins.sort(reverse=True) # O(1), 1 <= coins.length <= 12
-        prev = set([coins[0]])
-
-        for i in range(1, len(coins)):
-            result = []
-            for index, prevSum in enumerate(prev):
-                for k in range(index, i+1):
-                    currSum = coins[k]+prevSum
-                    result.append(currSum)
-            
-            if len(prev) > 1:
-                result.append(int(prevSum/i*(i+1)))
-            
-            currSum = coins[i]*(i+1)
-            result.append(currSum)
-            prev = result
-
-        print(prev)
-        return -1
-
+        count = 1
+        while True:
+            currSums = []
+            for i in range(len(prevSums)):
+                prevSum = prevSums[i]
+                for j in range(i, len(coins)):
+                    coin = coins[j]
+                    currSum = prevSum + coin
+                    if currSum < amount:
+                        currSums.append(currSum)
+                    elif currSum == amount:
+                        return count
+            if not currSums or currSums[-1] > amount:
+                return -1
+            count += 1
+            prevSums = currSums
+        
 
 
 coins = [1,2,5]
 amount = 11
 print(Solution().coinChange(coins, amount))
+coins = [2]
+amount = 3
+print(Solution().coinChange(coins, amount))
+coins = [1]
+amount = 0
+print(Solution().coinChange(coins, amount))
 
-# 4 3 2 1
+# 5 2 1 --> 11
 
-# 5
-# 55 52 22
-# 555 552 551 522 521 222 221 111
+# 1 coin
+# 5 
+# 2
+# 1
+
+# 2 coin
+# 55 
+# 52 
+# 51
+# 22
+# 21
+# 11 --> check if this is more than amount, if it is, return -1
+
+# 3 coin
+# 555
+# 525
+# 515 
+# 225
+# 215
+# 115
+# 552
+# 522
+# 512
+# 222
+# 212
+# 112
+# 551
+# 521
+# 511
+# 221
+# 211
+# 111
