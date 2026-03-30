fb7d5c88d91de66c350ca0a84716f0d5c1af061c
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-30 18:44:52 +0800

    longest-common-subsequence.py

---
+++ 2-D Dynamic Programming/unique-paths.py
@@ -0,0 +1,33 @@
+class Solution:
+    paths = {}
+
+    # Iterative
+    def uniquePaths(self, m: int, n: int) -> int:
+        for i in range(m):
+            self.paths[(i,0)] = 1
+        for j in range(n):
+            self.paths[(0,j)] = 1
+
+        for row in range(1, m):
+            for col in range(1, n):
+                self.paths[(row,col)] = self.paths[(row-1,col)] + self.paths[(row,col-1)]
+        
+        return self.paths[(m-1,n-1)]
+    
+    # Recursive
+    def uniquePaths(self, m: int, n: int) -> int:
+        for i in range(m):
+            self.paths[(i,0)] = 1
+        for j in range(n):
+            self.paths[(0,j)] = 1
+
+        def move(row, col):
+            if (row, col) in self.paths:
+                return self.paths[(row, col)]
+            self.paths[(row, col)] = move(row - 1, col) + move(row, col - 1)
+            return self.paths[(row, col)]
+
+        return move(m - 1, n - 1)
+
+print(Solution().uniquePaths(3, 2))
+print(Solution().uniquePaths(3, 7))
---
+++ HackerRank/a.py
@@ -0,0 +1,33 @@
+def isNonTrivialRotation(s1, s2):
+    if s1 == s2:
+        return False
+    startPositions = []
+    for i in range(len(s1)):
+        if s1[i] == s2[0]:
+            startPositions.append(i)
+    if not startPositions:
+        return False
+    print(startPositions)
+    for start in startPositions:
+        index1 = start
+        index2 = 0
+        
+        while index1 < len(s1) and index2 < len(s2):
+            if s1[index1] != s2[index2]:
+                break
+            print(s1[index1])
+            index1 += 1
+            index2 += 1
+        if index2 < len(s2):
+            index1 = 0
+        while index1 < len(s1) and index2 < len(s2):
+            if s1[index1] != s2[index2]:
+                break
+            print(s1[index1])
+            index1 += 1
+            index2 += 1
+        if index2 == len(s2):
+            return True
+    return False
+
+print(isNonTrivialRotation("abcdabcde", "cdeabcdab"))
