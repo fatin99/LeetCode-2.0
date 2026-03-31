de90ae864c68e82b08e8d7776d9655b7380c2d81
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-03-31 16:05:17 +0800

    target-sum

--- 1-D Dynamic Programming/coin-change.py
+++ 1-D Dynamic Programming/coin-change.py
@@ -1,7 +1,39 @@
 from collections import deque
 
 class Solution:
-    # DP Bottom Up (BFS)
+    # DP Bottom Up
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+    
+    # DP Top Down (Recursive DFS)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        memo = {}
+
+        def dfs(amount):
+            if amount == 0:
+                return 0
+            if amount in memo:
+                return memo[amount]
+
+            res = 1e9
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1 + dfs(amount - coin))
+
+            memo[amount] = res
+            return res
+
+        minCoins = dfs(amount)
+        return -1 if minCoins >= 1e9 else minCoins
+    
+    # DP Bottom Up (Iterative BFS)
     def coinChange(self, coins: List[int], amount: int) -> int:
         if amount == 0:
             return 0
---
+++ 1-D Dynamic Programming/target-sum.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        nums.sort(reverse=True) # O(1), 1 <= nums.length <= 20  
+        totalSum = sum(nums)
+        if target > totalSum or target < -totalSum:
+            return 0 
+
+        prevSums = {0: 1}
+        for i in range(0, len(nums)):
+            currSums = defaultdict(int)
+            nextSums = sum(nums[i:])
+            for prevSum, ways in prevSums.items():
+                add = prevSum + nextSums
+                if target <= add:
+                    currSum = prevSum + nums[i]
+                    currSums[currSum] += ways
+
+                subtract = prevSum - nextSums
+                if target >= subtract:
+                    currSum = prevSum - nums[i]
+                    currSums[currSum] += ways
+            prevSums = currSums 
+        return prevSums.get(target, 0)
+    
+nums = [1]
+target = 1
+print(Solution().findTargetSumWays(nums, target))
+
+nums = [1,0]
+target = 1
+print(Solution().findTargetSumWays(nums, target))
+    
+nums = [1,1,1,1,1]
+target = 3
+print(Solution().findTargetSumWays(nums, target))
+    
