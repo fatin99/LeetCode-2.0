5f2f37070f1ade0791a3060bf2d8128df5b22276
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-05-18 14:37:40 +0800

    minimum-window-with-characters

---
+++ Sliding Window/minimum-window-with-characters.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        res = ""
+        if len(t) > len(s) or len(s) == 0:
+            return res
+        char_map = {}
+        for char in t:
+            if char in char_map:
+                char_map[char] += 1
+            else:
+                char_map[char] = 1
+
+        substr = ""
+        start = 0
+        for end in range(1, len(s) + 1):
+            newChar = s[end - 1]
+            if newChar in char_map:
+                char_map[newChar] -= 1
+                if char_map[newChar] <= 0:  # reduce size of window
+                    while (
+                        all(value <= 0 for value in char_map.values())
+                        or s[start] not in char_map
+                    ):
+                        if s[start] in char_map:
+                            if char_map[s[start]] < 0:
+                                char_map[s[start]] += 1
+                            else:
+                                break
+                        start += 1
+            substr = s[start:end]
+            if all(value <= 0 for value in char_map.values()):
+                if res == "" or len(substr) < len(res):
+                    res = substr
+        return res
+
+
+s = "ADOBECODEBANC"
+t = "ABC"
+print(Solution().minWindow(s, t))
+s = "ab"
+t = "b"
+print(Solution().minWindow(s, t))
+s = "bbaa"
+t = "aba"
+print(Solution().minWindow(s, t))
