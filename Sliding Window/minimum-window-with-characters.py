5c6732b29b169bc7ad31b4fb9d13648e55a99975
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-05-18 14:59:31 +0800

    minimum-window-with-characters

--- Sliding Window/minimum-window-with-characters.py
+++ Sliding Window/minimum-window-with-characters.py
@@ -12,25 +12,27 @@ class Solution:
                 char_map[char] += 1
             else:
                 char_map[char] = 1
-
+        
+        count = 0
         start = 0
         for end in range(1, len(s) + 1):
             newChar = s[end - 1]
             if newChar in char_map:
                 char_map[newChar] -= 1
+                if char_map[newChar] <= 0:
+                    count += 1
+                
                 if char_map[newChar] <= 0:  # reduce size of window
-                    while (
-                        all(value <= 0 for value in char_map.values())
-                        or s[start] not in char_map
-                    ):
+                    while count >= len(char_map):
                         if s[start] in char_map:
                             if char_map[s[start]] < 0:
                                 char_map[s[start]] += 1
+                                count -= 1
                             else:
                                 break
                         start += 1
-            
-            if all(value <= 0 for value in char_map.values()):
+
+            if count == len(char_map):
                 if res == "" or end-start < len(res):
                     res = s[start:end]
         return res
@@ -39,9 +41,9 @@ class Solution:
 s = "ADOBECODEBANC"
 t = "ABC"
 print(Solution().minWindow(s, t))
-s = "ab"
-t = "b"
-print(Solution().minWindow(s, t))
-s = "bbaa"
-t = "aba"
-print(Solution().minWindow(s, t))
+# s = "ab"
+# t = "b"
+# print(Solution().minWindow(s, t))
+# s = "bbaa"
+# t = "aba"
+# print(Solution().minWindow(s, t))
