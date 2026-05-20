1b2064ee90930f46be1d457d1380f195b8aad419
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-05-20 22:06:59 +0800

    merge-k-sorted-linked-lists

--- Linked List/merge-k-sorted-linked-lists.py
+++ Linked List/merge-k-sorted-linked-lists.py
@@ -11,7 +11,7 @@ class Solution:
         curr = head
 
         if len(lists) == 0:
-            return head
+            return None
         curr.next = lists[0]
         curr = curr.next
 
@@ -33,6 +33,35 @@ class Solution:
             curr = curr.next
 
         return head.next
+
+    # Divide And Conquer (Iteration)
+    # O(nlogk) time and O(k) space
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists) == 0:
+            return None
+
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+
+                head = ListNode(0)
+                curr = head
+
+                while l1 and l2:
+                    if l1.val < l2.val:
+                        curr.next = l1
+                        l1 = l1.next
+                    else:
+                        curr.next = l2
+                        l2 = l2.next
+                    curr = curr.next
+                curr.next = l1 if l1 else l2
+                mergedLists.append(head.next)
+
+            lists = mergedLists
+        return lists[0]
 
 
 def list_to_linked_list(items):
