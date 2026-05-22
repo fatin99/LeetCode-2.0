c112d88568948261cbbe6754f435f54725d63c00
Fatin Nabilah <42372470+fatin99@users.noreply.github.com>
2026-05-22 08:54:26 +0800

    word-search-ii

--- Tries/search-for-word-ii.py
+++ Tries/search-for-word-ii.py
@@ -27,29 +27,30 @@ class Solution:
     res = set()
     board = None
 
-    def dfs(self, x, y, curr):
-        char = self.board[x][y]
-        if (x, y) in self.seen or char not in curr.children:
-            return
+    # Recurive
+    # def dfs(self, x, y, curr):
+    #     char = self.board[x][y]
+    #     if (x, y) in self.seen or char not in curr.children:
+    #         return
 
-        curr = curr.children[char]
-        if curr.is_end_of_word:
-            self.res.add(curr.path)
-        if not curr.children:
-            return
+    #     curr = curr.children[char]
+    #     if curr.is_end_of_word:
+    #         self.res.add(curr.path)
+    #     if not curr.children:
+    #         return
 
-        self.seen.add((x, y))
+    #     self.seen.add((x, y))
 
-        if x > 0:
-            self.dfs(x - 1, y, curr)
-        if x < len(self.board) - 1:
-            self.dfs(x + 1, y, curr)
-        if y > 0:
-            self.dfs(x, y - 1, curr)
-        if y < len(self.board[x]) - 1:
-            self.dfs(x, y + 1, curr)
+    #     if x > 0:
+    #         self.dfs(x - 1, y, curr)
+    #     if x < len(self.board) - 1:
+    #         self.dfs(x + 1, y, curr)
+    #     if y > 0:
+    #         self.dfs(x, y - 1, curr)
+    #     if y < len(self.board[x]) - 1:
+    #         self.dfs(x, y + 1, curr)
 
-        self.seen.discard((x, y))
+    #     self.seen.discard((x, y))
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         self.board = board
@@ -59,38 +60,44 @@ class Solution:
         for word in words:
             trie.insert(word)
 
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                self.dfs(i, j, trie.root)
-
-        # need to use recursive dfs
-        # seen | {(x,y)} copies the whole set on every push
-        # (O(t) per step), adding a t factor to time complexity
-        # carrying seen set of size up to t → O(3^t · t) space
         # for i in range(len(board)):
         #     for j in range(len(board[i])):
-        #         cells = [(i,j,set(),trie.root)]
-        #         while len(cells) > 0:
-        #             cell = cells.pop()
-        #             x = cell[0]
-        #             y = cell[1]
-        #             seen = cell[2]
-        #             curr = cell[3]
-        #             if (not curr.children) or ((x,y) in seen):
-        #                 continue
-        #             char = board[x][y]
-        #             if char in curr.children:
-        #                 curr = curr.children[char]
-        #                 if x > 0:
-        #                     cells.append((x-1, y, seen | {(x,y)}, curr))
-        #                 if x < len(board)-1:
-        #                     cells.append((x+1, y , seen | {(x,y)}, curr))
-        #                 if y > 0:
-        #                     cells.append((x, y-1 , seen | {(x,y)}, curr))
-        #                 if y < len(board[i])-1:
-        #                     cells.append((x, y+1 , seen | {(x,y)}, curr))
-        #             if curr.is_end_of_word:
-        #                 res.add(curr.path)
+        #         self.dfs(i, j, trie.root)
+
+        # Iterative
+        for i in range(len(board)):
+            for j in range(len(board[i])):
+                cells = [(i, j, trie.root)]
+                while len(cells) > 0:
+                    cell = cells.pop()
+                    x = cell[0]
+                    y = cell[1]
+                    curr = cell[2]
+                    if curr is None:  # backtrack
+                        self.seen.discard((x, y))
+                        continue
+
+                    char = board[x][y]
+                    if (x, y) in self.seen or char not in curr.children:
+                        continue
+                    curr = curr.children[char]
+                    if curr.is_end_of_word:
+                        self.res.add(curr.path)
+                    if not curr.children:
+                        continue
+
+                    self.seen.add((x, y))
+                    cells.append((x, y, None))  # exit marker
+                    # it will backtrack to here after exploring all surrounding
+                    # cells that are pushed onto the stack after it
+                    if x > 0:
+                        cells.append((x - 1, y, curr))
+                    if x < len(board) - 1:
+                        cells.append((x + 1, y, curr))
+                    if y > 0:
+                        cells.append((x, y - 1, curr))
+                    if y < len(board[x]) - 1:
+                        cells.append((x, y + 1, curr))
         return list(self.res)
 
 
